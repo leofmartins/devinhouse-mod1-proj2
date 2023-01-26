1236e64d5654a96a00c6d809d471847a324c6f6c
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-01-26 20:25:31 -0300

    Prepara o componente para validação dos dados do formulário

--- src/app/features/add-appointment/add-appointment.component.ts
+++ src/app/features/add-appointment/add-appointment.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'lab-add-appointment',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-appointment.component.css']
 })
 export class AddAppointmentComponent {
-
+  addAppointmentForm = this.fb.group([{
+    subject: [null, Validators.compose([
+      Validators.required, Validators.minLength(8), Validators.maxLength(64)
+    ])],
+    date: [null, Validators.required],
+    time: [null, Validators.required],
+    description: [null, Validators.compose([
+      Validators.required, Validators.minLength(16), Validators.maxLength(1024)
+    ])],
+    medication: null,
+    precaution: [null, Validators.compose([
+      Validators.required, Validators.minLength(16), Validators.maxLength(256)
+    ])]
+  }]);
+  constructor(private fb: FormBuilder) { }
 }
