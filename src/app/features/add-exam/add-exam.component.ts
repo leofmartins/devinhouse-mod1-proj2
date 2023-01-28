a7947528a9a4ae1743f511c220e19b277e0da49e
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-01-27 21:16:25 -0300

    Estrutura base do formulário de cadastro de exames

--- src/app/features/add-exam/add-exam.component.ts
+++ src/app/features/add-exam/add-exam.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'lab-add-exam',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-exam.component.css']
 })
 export class AddExamComponent {
+  addExamForm = this.fb.group({
+    examName: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(68)
+      ]
+    )],
+    examDate: [
+      null,
+      Validators.required],
+    examTime: [
+      null,
+      Validators.required],
+    examType: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(32)
+      ])],
+    labName: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(64)
+      ])],
+    reportUrl: [null],
+    results: [
+      null,
+      Validators.compose([
+        Validators.required,
+        Validators.minLength(16),
+        Validators.maxLength(1024)
+    ])]
+  });
 
+  constructor(private fb: FormBuilder) { }
 }
