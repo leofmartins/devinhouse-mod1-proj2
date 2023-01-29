5d5b30d7ae731dbe551f02ef6ea9f2ccc5757b51
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-01-29 17:31:45 -0300

    Deleta mock de dados.

--- src/app/features/home-page/home-page.component.ts
+++ src/app/features/home-page/home-page.component.ts
@@ -8,13 +8,37 @@ import { PeopleService } from "@services/people-service";
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  people: People = [];
+  people: People | undefined;
 
   constructor(private peopleService: PeopleService) {
   }
 
   ngOnInit() {
     this.peopleService.getPeople()
-      .subscribe(people => this.people = people);
+      .subscribe(
+        people => this.people = people
+      );
+  }
+  getPeopleLength(): number {
+    if (this.people) {
+      return this.people.length;
+    }
+    return 0;
+  }
+
+  getAppointMentCount(): number {
+    let numberOfAppointments = 0;
+    this.people?.forEach(
+      person => numberOfAppointments += person.appointment.length
+    )
+    return numberOfAppointments
+  }
+
+  getExamsCount(): number {
+    let numberOfExams = 0;
+    this.people?.forEach(
+      person => numberOfExams += person.exams.length
+    );
+    return numberOfExams
   }
 }
