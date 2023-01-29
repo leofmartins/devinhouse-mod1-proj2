0a947642366f556570fc7b9d76895303af8d1b1f
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-01-29 17:31:58 -0300

    Revert "Deleta mock de dados."
    This reverts commit 5d5b30d7ae731dbe551f02ef6ea9f2ccc5757b51.

--- src/app/features/home-page/home-page.component.ts
+++ src/app/features/home-page/home-page.component.ts
@@ -8,37 +8,13 @@ import { PeopleService } from "@services/people-service";
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-  people: People | undefined;
+  people: People = [];
 
   constructor(private peopleService: PeopleService) {
   }
 
   ngOnInit() {
     this.peopleService.getPeople()
-      .subscribe(
-        people => this.people = people
-      );
-  }
-  getPeopleLength(): number {
-    if (this.people) {
-      return this.people.length;
-    }
-    return 0;
-  }
-
-  getAppointMentCount(): number {
-    let numberOfAppointments = 0;
-    this.people?.forEach(
-      person => numberOfAppointments += person.appointment.length
-    )
-    return numberOfAppointments
-  }
-
-  getExamsCount(): number {
-    let numberOfExams = 0;
-    this.people?.forEach(
-      person => numberOfExams += person.exams.length
-    );
-    return numberOfExams
+      .subscribe(people => this.people = people);
   }
 }
