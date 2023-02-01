137da269db03329fe02f3bc34dc5c8909b4f586b
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-01-31 22:34:10 -0300

    Inclui validação dos campos e-mails e senha da tela de login.

--- src/app/features/login-page/login-page.component.ts
+++ src/app/features/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'lab-login-page',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-page.component.css']
 })
 export class LoginPageComponent {
-
+  loginForm = this.fb.group({
+    email: [null, Validators.compose([
+      Validators.required, Validators.email
+    ])],
+    password: [null, Validators.compose([
+      Validators.required, Validators.minLength(8)
+    ])]
+  })
+  constructor(private fb: FormBuilder) {
+  }
 }
