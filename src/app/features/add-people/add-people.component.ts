a87f42a99af058e0c4a6bd659b9f7dc6909235b4
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-02-02 22:06:08 -0300

    Implementar spinner no form par ao caso de estar buscando infos da pessoa no bd.

--- src/app/features/add-people/add-people.component.ts
+++ src/app/features/add-people/add-people.component.ts
@@ -1,48 +1,21 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ViacepService } from "./services/viacep.service";
 import { ViaCep } from "@shared/interfaces";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'lab-add-people',
   templateUrl: './add-people.component.html',
   styleUrls: ['./add-people.component.css']
 })
-export class AddPeopleComponent {
-  addPeopleForm = this.fb.group({
-    allergies: [null],
-    birthTown: [null, Validators.required],
-    birthdate: [null, Validators.required],
-    city: [null, Validators.required],
-    complement: [null],
-    cpf: [null, Validators.required],
-    editToggle: ['false', Validators.required],
-    email: [null, Validators.compose([
-      Validators.required, Validators.email
-    ])],
-    emergencyContact: [null, Validators.required],
-    gender: [null, Validators.required],
-    healthCareNumber: [null],
-    healthInsurance: [null],
-    houseNumber: [null, Validators.required],
-    maritalStatus: [null, Validators.required],
-    name: [null, Validators.compose([
-      Validators.required, Validators.minLength(8), Validators.maxLength(64)
-    ])],
-    neighborhood: [null, Validators.required],
-    phoneNumber: [null, Validators.required],
-    postalCode: [null, Validators.compose([
-      Validators.required, Validators.minLength(8), Validators.maxLength(8)])
-    ],
-    reference: [null],
-    rg: [null, Validators.compose([
-      Validators.required, Validators.maxLength(20)
-    ])],
-    specialCares: [null],
-    state: [null, Validators.required],
-    street: [null, Validators.required],
-    town: [null, Validators.required]
-  });
+export class AddPeopleComponent implements OnInit {
+  addPeopleForm!: FormGroup;
+  id?: string;
+  title!: string;
+  loading = true;
+  submmited = false;
+  submmiting = false;
 
   genders = ['Masculino', 'Feminino'];
 
@@ -67,7 +40,56 @@ export class AddPeopleComponent {
     siafi: ""
   }
 
-  constructor(private fb: FormBuilder, private viaCepService: ViacepService) {}
+  constructor(
+    private fb: FormBuilder,
+    private viaCepService: ViacepService,
+    private rout: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.id = this.rout.snapshot.params['id'];
+    this.addPeopleForm = this.fb.group({
+      allergies: [null],
+      birthTown: [null, Validators.required],
+      birthdate: [null, Validators.required],
+      city: [null, Validators.required],
+      complement: [null],
+      cpf: [null, Validators.required],
+      editToggle: ['false', Validators.required],
+      email: [null, Validators.compose([
+        Validators.required, Validators.email
+      ])],
+      emergencyContact: [null, Validators.required],
+      gender: [null, Validators.required],
+      healthCareNumber: [null],
+      healthInsurance: [null],
+      houseNumber: [null, Validators.required],
+      maritalStatus: [null, Validators.required],
+      name: [null, Validators.compose([
+        Validators.required, Validators.minLength(8), Validators.maxLength(64)
+      ])],
+      neighborhood: [null, Validators.required],
+      phoneNumber: [null, Validators.required],
+      postalCode: [null, Validators.compose([
+        Validators.required, Validators.minLength(8), Validators.maxLength(8)])
+      ],
+      reference: [null],
+      rg: [null, Validators.compose([
+        Validators.required, Validators.maxLength(20)
+      ])],
+      specialCares: [null],
+      state: [null, Validators.required],
+      street: [null, Validators.required],
+      town: [null, Validators.required]
+    });
+
+    this.title = "Preencha os campos para cadastrar";
+
+    if (this.id) {
+      this.title = `Editando o cadastro de [Fulano]`;
+    }
+
+  }
 
   onSubmit(): void {
     alert('Thanks!');
