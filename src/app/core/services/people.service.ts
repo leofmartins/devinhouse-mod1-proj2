5fba6593973fbc1e384a52c5a098d2c1d7b423ac
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-02-05 12:52:14 -0300

    Implementa utilização do serviço de editar pessoa no componente de cadastro.

--- src/app/core/services/people.service.ts
+++ src/app/core/services/people.service.ts
@@ -27,7 +27,7 @@ export class PeopleService {
     return this.http.post<Person>(this.peopleUrl, person, this.httpOptions);
   }
 
-  editPerson(person: Person): Observable<Person> {
-    return this.http.put<Person>(this.peopleUrl, person, this.httpOptions);
+  editPerson(person: Person, id: string): Observable<Person> {
+    return this.http.put<Person>(`${this.peopleUrl}/${id}`, person, this.httpOptions);
   }
 }
